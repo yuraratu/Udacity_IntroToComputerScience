b256cca0651244d579961ec47561c6b81c0eb7d0
Yurii Ratushnyi <yuriy.ratushny@gmail.com>
2016-09-19 18:26:26 +0100

    Keep doing

--- Lesson 3 How to Manage Data/Lesson 3.1.py
+++ Lesson 3 How to Manage Data/Lesson 3.1.py
@@ -127,3 +127,112 @@ print('##################################################')
 ##########################################################
 
 
+# Define a procedure, measure_udacity,
+# that takes as its input a list of strings,
+# and returns a number that is a count
+# of the number of elements in the input
+# list that start with the uppercase
+# letter 'U'.
+
+
+def measure_udacity(p):
+    count = 0
+    for i in p:
+        if i[0] == 'U':
+            count += 1
+    return count
+
+
+print(measure_udacity(['Dave', 'Sebastian', 'Katy']))
+# >>> 0
+
+print(measure_udacity(['Umika', 'Umberto']))
+# >>> 2
+
+
+print('##################################################')
+##########################################################
+
+
+# Define a procedure, find_element,
+# that takes as its inputs a list
+# and a value of any type, and
+# returns the index of the first
+# element in the input list that
+# matches the value.
+
+# If there is no matching element,
+# return -1.
+
+
+def find_element(search, value):
+    for i in search:
+        if i == value:
+            return search.index(value)
+    return -1
+
+
+print(find_element([1, 2, 3], 3))
+# >>> 2
+
+print(find_element(['alpha', 'beta'], 'gamma'))
+# >>> -1
+
+
+print('##################################################')
+##########################################################
+
+
+# Define a procedure, find_element,
+# using index that takes as its
+# inputs a list and a value of any
+# type, and returns the index of
+# the first element in the input
+# list that matches the value.
+
+# If there is no matching element,
+# return -1.
+
+
+def find_element_short(search, value):
+    if value in search:
+        return search.index(value)
+    return -1
+
+
+print(find_element_short([1, 2, 3], 3))
+# >>> 2
+
+print(find_element_short(['alpha', 'beta'], 'gamma'))
+# >>> -1
+
+
+print('##################################################')
+##########################################################
+
+
+# Define a procedure, union,
+# that takes as inputs two lists.
+# It should modify the first input
+# list to be the set union of the two
+# lists. You may assume the first list
+# is a set, that is, it contains no
+# repeated elements.
+
+
+def union(list1, list2):
+    for i in list2:
+        if i not in list1:
+            list1.append(i)
+
+
+# To test, uncomment all lines
+# below except those beginning with >>>.
+
+a = [1, 2, 3]
+b = [2, 4, 6]
+union(a, b)
+print(a)
+# >>> [1,2,3,4,6]
+print(b)
+# >>> [2,4,6]
