ac09fca798d591b2caaa070e2d717796c16c7f38
Yurii Ratushnyi <yuriy.ratushny@gmail.com>
2016-10-13 18:38:53 +0100

    Kepp doing

--- Lesson 4 Responding to Queries/Lesson 4.2.2 Counting Clicks.py
+++ Lesson 4 Responding to Queries/Lesson 4.2.2 Counting Clicks.py
@@ -1,5 +1,5 @@
 # 2 Gold Stars
-
+#
 # One way search engines rank pages
 # is to count the number of times a
 # searcher clicks on a returned link.
@@ -7,31 +7,31 @@
 # the query thought this was a useful
 # link for the query, so it should be
 # higher in the rankings next time.
-
+#
 # (In Unit 6, we will look at a different
 # way of ranking pages that does not depend
 # on user clicks.)
-
+#
 # Modify the index such that for each url in a
 # list for a keyword, there is also a number
 # that counts the number of times a user
 # clicks on that link for this keyword.
-
+#
 # The result of lookup(index,keyword) should
 # now be a list of url entries, where each url
 # entry is a list of a url and a number
 # indicating the number of times that url
 # was clicked for this query keyword.
-
+#
 # You should define a new procedure to simulate
 # user clicks for a given link:
-
+#
 # record_user_click(index,word,url)
-
+#
 # that modifies the entry in the index for
 # the input word by increasing the count associated
 # with the url by 1.
-
+#
 # You also will have to modify add_to_index
 # in order to correctly create the new data
 # structure, and to prevent the repetition of
---
+++ Lesson 4 Responding to Queries/Lesson 4.3.3 Download Calculator.py
@@ -0,0 +1,81 @@
+# Write a procedure download_time which takes as inputs a file size, the
+# units that file size is given in, bandwidth and the units for
+# bandwidth (excluding per second) and returns the time taken to download
+# the file.
+# Your answer should be a string in the form
+# "<number> hours, <number> minutes, <number> seconds"
+
+# Some information you might find useful is the number of bits
+# in kilobits (kb), kilobytes (kB), megabits (Mb), megabytes (MB),
+# gigabits (Gb), gigabytes (GB) and terabits (Tb), terabytes (TB).
+
+# print 2 ** 10      # one kilobit, kb
+# print 2 ** 10 * 8  # one kilobyte, kB
+#
+# print 2 ** 20      # one megabit, Mb
+# print 2 ** 20 * 8  # one megabyte, MB
+#
+# print 2 ** 30      # one gigabit, Gb
+# print 2 ** 30 * 8  # one gigabyte, GB
+#
+# print 2 ** 40      # one terabit, Tb
+# print 2 ** 40 * 8  # one terabyte, TB
+
+# Often bandwidth is given in megabits (Mb) per second whereas file size
+# is given in megabytes (MB).
+
+
+def convert_seconds(n):
+    hours = int(n / 3600)
+    result = str(hours)
+    if hours == 1:
+        result += ' hour, '
+    else:
+        result += ' hours, '
+
+    minutes = int((n % 3600) / 60)
+    result += str(minutes)
+    if minutes == 1:
+        result += ' minute, '
+    else:
+        result += ' minutes, '
+
+    seconds = n % 60
+    result += str(seconds)
+    if seconds == 1:
+        result += ' second'
+    else:
+        result += ' seconds'
+
+    return result
+
+
+def download_time(size, s_unit, bandwidth, b_unit):
+    mults = [['kb', 2 ** 10], ['Mb', 2 ** 20], ['Gb', 2 ** 30], ['Tb', 2 ** 40],
+             ['kB', 2 ** 10 * 8], ['MB', 2 ** 20 * 20], ['GB', 2 ** 30 * 8], ['TB', 2 ** 40 * 8]]
+
+    size_bytes = size * mults[1][mults[0].index(s_unit)]
+    bandwidth_bytes = bandwidth * mults[1][mults[0].index(s_unit)]
+
+    time = size_bytes / bandwidth_bytes
+
+    return convert_seconds(time)
+
+
+print(download_time(1024,'kB', 1, 'MB'))
+# >>> 0 hours, 0 minutes, 1 second
+
+print(download_time(1024,'kB', 1, 'Mb'))
+# >>> 0 hours, 0 minutes, 8 seconds  # 8.0 seconds is also acceptable
+
+print(download_time(13,'GB', 5.6, 'MB'))
+# >>> 0 hours, 39 minutes, 37.1428571429 seconds
+
+print(download_time(13,'GB', 5.6, 'Mb'))
+# >>> 5 hours, 16 minutes, 57.1428571429 seconds
+
+print(download_time(10,'MB', 2, 'kB'))
+# >>> 1 hour, 25 minutes, 20 seconds  # 20.0 seconds is also acceptable
+
+print(download_time(10,'MB', 2, 'kb'))
+# >>> 11 hours, 22 minutes, 40 seconds  # 40.0 seconds is also acceptable
